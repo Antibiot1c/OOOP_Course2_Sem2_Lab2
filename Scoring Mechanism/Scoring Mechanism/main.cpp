d258449faa0cbe0aece01b0ce4c171f154ec4a96
Antibiot1c <112926321+Antibiot1c@users.noreply.github.com>
2023-03-23 23:51:22 +0200

    Update main.cpp
    Interface improvements

--- Scoring Mechanism/Scoring Mechanism/main.cpp
+++ Scoring Mechanism/Scoring Mechanism/main.cpp
@@ -26,6 +26,14 @@ public:
 
     // Add scores for a student
     void add_scores(string student, vector<int> student_scores) {
+        // Check if any of the scores are negative
+        for (int score : student_scores) {
+            if (score < 0) {
+                cout << "Error: negative score detected. Please enter a non-negative score." << endl;
+                return;
+            }
+        }
+        // Add scores if all scores are non-negative
         scores[student] = student_scores;
     }
 
@@ -70,36 +78,26 @@ int main() {
     TaskManager task_manager;
 
     // Add tasks
-    task_manager.add_task("Mathematics", 10);
-    task_manager.add_task("Physics", 15);
-    task_manager.add_task("History", 5);
+    task_manager.add_task("Task 1", 10);
+    task_manager.add_task("Task 2", 20);
+    task_manager.add_task("Task 3", 15);
 
-    // Input students and scores
-    int num_students;
-    cout << "Enter number of students in this group: ";
-    cin >> num_students;
+    // Add scores for students
+    task_manager.add_scores("Alice", { 8, 15, 10 });
+    task_manager.add_scores("Bob", { 12, 18, 20 });
+    task_manager.add_scores("Charlie", { 10, -5, 15 }); // Negative score
 
-    for (int i = 1; i <= num_students; i++) {
-        string student_name;
-        cout << "Enter student name " << i << ": ";
-        cin >> student_name;
-
-        vector<int> student_scores;
-        for (int j = 0; j < task_manager.num_tasks(); j++) {
-            int score;
-            cout << "Enter " << task_manager.get_task(j).name << " score for " << student_name << ": ";
-            cin >> score;
-            student_scores.push_back(score);
-        }
-
-        task_manager.add_scores(student_name, student_scores);
-    }
-
+    // Calculate results
+    cout << "Results:" << endl;
+    cout << "Alice: " << task_manager.calculate_result("Alice") << endl;
+    cout << "Bob: " << task_manager.calculate_result("Bob") << endl;
+    cout << "Charlie: " << task_manager.calculate_result("Charlie") << endl;
 
     // Save scores to file
     string file_name;
     cout << "Enter name of file to save scores to: ";
-    cin >> file_name; ofstream out_file(file_name);
+    cin >> file_name;
+    ofstream out_file(file_name);
     if (out_file) {
         // Write task names
         out_file << "Student Name,";
@@ -116,26 +114,21 @@ int main() {
         for (auto score_pair : scores) {
             out_file << score_pair.first << ",";
             vector<int> student_scores = score_pair.second;
-            for (int i = 0; i < student_scores.size(); i++) {
-                out_file << student_scores[i];
-                if (i < student_scores.size() - 1) {
-                    out_file << ",";
+       
+                for (int i = 0; i < student_scores.size(); i++) {
+                    out_file << student_scores[i];
+                    if (i < student_scores.size() - 1) {
+                        out_file << ",";
+                    }
                 }
-            }
             out_file << endl;
         }
         out_file.close();
         cout << "Scores saved to " << file_name << endl;
     }
     else {
-        cout << "Unable to open file " << file_name << " for writing." << endl;
+        cout << "Error: could not open file " << file_name << endl;
     }
 
-    // Calculate and print results for each student
-    map<string, vector<int>> scores = task_manager.get_scores();
-    for (auto score_pair : scores) {
-        string student_name = score_pair.first;
-        double result = task_manager.calculate_result(student_name);
-        cout  << student_name << " result: " << result << endl;
-    }
+    return 0;
 }
